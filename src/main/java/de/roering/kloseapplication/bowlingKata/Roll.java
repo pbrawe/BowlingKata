e05439719820372172ded0dbb8b6e923aba74fce
pbrawe <philipproering94@gmail.com>
2022-01-23 18:40:42 +0100

    implemented basic Roll

---
+++ src/main/java/de/roering/kloseapplication/bowlingKata/Roll.java
@@ -0,0 +1,12 @@
+package de.roering.kloseapplication.bowlingKata;
+
+public class Roll {
+    private int hitPins;
+    public Roll(int hitPins) {
+        this.hitPins = hitPins;
+    }
+
+    public int getHitPins() {
+        return this.hitPins;
+    }
+}
