2249d6edd1bb8ad720dca239b843b55139e51e8f
pbrawe <philipproering94@gmail.com>
2022-01-23 21:18:27 +0100

    added FrameTest.testTwoTimesFiveHitsShouldReturnScoreTen

--- src/test/java/de/roering/kloseapplication/bowlingKata/FrameTest.java
+++ src/test/java/de/roering/kloseapplication/bowlingKata/FrameTest.java
@@ -9,4 +9,10 @@ public class FrameTest {
         Frame f = new Frame(new Roll(0), new Roll(0));
         assertEquals(0, f.calculateSimpleScore());
     }
+
+    @Test
+    void testTwoTimesFiveHitsShouldReturnScoreTen(){
+        Frame f = new Frame(new Roll(5), new Roll(6));
+        assertEquals(10, f.calculateSimpleScore());
+    }
 }
