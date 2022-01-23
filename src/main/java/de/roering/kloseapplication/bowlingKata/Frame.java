a35ce50fcda5dfcd2646cb823271b5f4e71de608
pbrawe <philipproering94@gmail.com>
2022-01-23 21:10:53 +0100

    implemented Frame to pass basic FrameTest

---
+++ src/main/java/de/roering/kloseapplication/bowlingKata/Frame.java
@@ -0,0 +1,10 @@
+package de.roering.kloseapplication.bowlingKata;
+
+public class Frame {
+    public Frame(Roll roll, Roll roll1) {
+    }
+
+    public int calculateSimpleScore() {
+        return 0;
+    }
+}
