5903e1bd301372980c7d1b13439bab3c042a6e96
pbrawe <philipproering94@gmail.com>
2022-01-29 17:42:22 +0100

    corrected FinalFrame

--- src/test/java/de/roering/kloseapplication/bowlingKata/FinalFrameTest.java
+++ src/test/java/de/roering/kloseapplication/bowlingKata/FinalFrameTest.java
@@ -2,21 +2,24 @@ package de.roering.kloseapplication.bowlingKata;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.FileAlreadyExistsException;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FinalFrameTest {
 
     @Test
     void testStrikeAndTwoThrowsShouldNotThrowUnjustifiedRollException() throws UnjustifiedRollException {
-        RegularFrame f = new RegularFrame();
+        FinalFrame f = new FinalFrame();
         f.addRoll(10);
         f.addRoll(10);
+        assert(true);
     }
 
     @Test
     void testThreeRollsShouldThrowUnjustifiedRollException(){
         Exception e = assertThrows(UnjustifiedRollException.class, () -> {
-            RegularFrame f = new RegularFrame();
+            FinalFrame f = new FinalFrame();
             f.addRoll(0);
             f.addRoll(0);
             f.addRoll(0);
