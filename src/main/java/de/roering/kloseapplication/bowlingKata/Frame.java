0bdd4ef4a7e1885bff29a555eb460d04f44e5f31
pbrawe <philipproering94@gmail.com>
2022-01-29 15:25:23 +0100

    pass FrameTest.testStrikeShouldReturnSimpleScoreTen

--- src/main/java/de/roering/kloseapplication/bowlingKata/Frame.java
+++ src/main/java/de/roering/kloseapplication/bowlingKata/Frame.java
@@ -1,13 +1,17 @@
 package de.roering.kloseapplication.bowlingKata;
 
 public class Frame {
+    private final static int STRIKE_PINS = 10;
     private Roll roll1, roll2;
     public Frame(Roll roll1, Roll roll2) {
         this.roll1 = roll1;
         this.roll2 = roll2;
     }
 
+    private int getSecondRollHitPins(){
+        return roll1.getHitPins() == STRIKE_PINS ? 0 : roll2.getHitPins();
+    }
     public int calculateSimpleScore() {
-        return roll1.getHitPins() + roll2.getHitPins();
+        return roll1.getHitPins() + getSecondRollHitPins();
     }
 }
