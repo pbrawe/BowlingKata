469a8fc3a12f0bc46a22b54ae4cd6642eefdbd34
pbrawe <philipproering94@gmail.com>
2022-01-29 18:04:22 +0100

    implement testThreeStrikesShouldReturnSimpleScoreThirty

--- src/test/java/de/roering/kloseapplication/bowlingKata/FinalFrameTest.java
+++ src/test/java/de/roering/kloseapplication/bowlingKata/FinalFrameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.file.FileAlreadyExistsException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FinalFrameTest {
@@ -24,5 +25,14 @@ public class FinalFrameTest {
             f.addRoll(0);
             f.addRoll(0);
         });
+    }
+
+    @Test
+    void testThreeStrikesShouldReturnSimpleScoreThirty() throws UnjustifiedRollException {
+        FinalFrame f = new FinalFrame();
+        f.addRoll(10);
+        f.addRoll(10);
+        f.addRoll(10);
+        assertEquals(30, f.calculateSimpleScore());
     }
 }
