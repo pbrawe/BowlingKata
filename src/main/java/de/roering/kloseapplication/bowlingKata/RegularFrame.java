bdf3f76020cc60117837242c4c789e832d3a4c77
pbrawe <philipproering94@gmail.com>
2022-01-29 16:37:52 +0100

    renamed Frame to RegularFrame

--- src/main/java/de/roering/kloseapplication/bowlingKata/RegularFrame.java
+++ src/main/java/de/roering/kloseapplication/bowlingKata/RegularFrame.java
@@ -1,12 +1,12 @@
 package de.roering.kloseapplication.bowlingKata;
 
-public class Frame {
+public class RegularFrame {
     private final static int STRIKE_PINS = 10;
     private Roll roll1, roll2;
-    public Frame(Roll roll1) {
+    public RegularFrame(Roll roll1) {
         this.roll1 = roll1;
     }
-    public Frame(Roll roll1, Roll roll2) throws UnjustifiedThrowException {
+    public RegularFrame(Roll roll1, Roll roll2) throws UnjustifiedThrowException {
         if (roll1.getHitPins() == STRIKE_PINS) throw new UnjustifiedThrowException();
         this.roll1 = roll1;
         this.roll2 = roll2;
--- src/test/java/de/roering/kloseapplication/bowlingKata/RegularFrameTest.java
+++ src/test/java/de/roering/kloseapplication/bowlingKata/RegularFrameTest.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class FrameTest {
+public class RegularFrameTest {
     public int getRandomNumberInRange(int min, int max) {
         Random random = new Random();
         return random.nextInt(max - min) + min;
@@ -13,13 +13,13 @@ public class FrameTest {
 
     @Test
     void testNoHitsShouldReturnSimpleScoreZero() throws UnjustifiedThrowException {
-        Frame f = new Frame(new Roll(0), new Roll(0));
+        RegularFrame f = new RegularFrame(new Roll(0), new Roll(0));
         assertEquals(0, f.calculateSimpleScore());
     }
 
     @Test
     void testTwoTimesFiveHitsShouldReturnSimpleScoreTen() throws UnjustifiedThrowException {
-        Frame f = new Frame(new Roll(5), new Roll(5));
+        RegularFrame f = new RegularFrame(new Roll(5), new Roll(5));
         assertEquals(10, f.calculateSimpleScore());
     }
 
@@ -30,21 +30,21 @@ public class FrameTest {
         while(rand == 10){
             rand = new Random(10).nextInt() + 1;
         }
-        Frame f = new Frame(new Roll(rand), new Roll(10 - rand));
+        RegularFrame f = new RegularFrame(new Roll(rand), new Roll(10 - rand));
         assertEquals(10, f.calculateSimpleScore());
     }
 
     @Test
     void testStrikeAndTwoThrowsShouldThrowUnjustifiedThrowException(){
         Exception e = assertThrows(UnjustifiedThrowException.class, () -> {
-            Frame f = new Frame(new Roll(10), new Roll(10));
+            RegularFrame f = new RegularFrame(new Roll(10), new Roll(10));
         });
     }
 
     @Test
     void testStrikeShouldReturnSimpleScoreTen(){
         final int RAND = getRandomNumberInRange(1, 10);
-        Frame f = new Frame(new Roll(10));
+        RegularFrame f = new RegularFrame(new Roll(10));
         assertEquals(10, f.calculateSimpleScore());
     }
 }
