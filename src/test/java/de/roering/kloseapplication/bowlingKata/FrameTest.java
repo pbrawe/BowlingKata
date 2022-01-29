95c4206fc2f78a3cecc4ee6ec1f74475f43e0d98
pbrawe <philipproering94@gmail.com>
2022-01-29 14:56:08 +0100

    added FrameTest.testRandomSpareShouldReturnSimpleScoreTen, FrameTest.testStrikeShouldReturnSimpleScoreTen

--- src/test/java/de/roering/kloseapplication/bowlingKata/FrameTest.java
+++ src/test/java/de/roering/kloseapplication/bowlingKata/FrameTest.java
@@ -1,6 +1,7 @@
 package de.roering.kloseapplication.bowlingKata;
 
 import org.junit.jupiter.api.Test;
+import java.util.Random;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FrameTest {
@@ -10,7 +11,7 @@ public class FrameTest {
     }
 
     @Test
-    void testNoHitsShouldReturnScoreZero(){
+    void testNoHitsShouldReturnSimpleScoreZero(){
         Frame f = new Frame(new Roll(0), new Roll(0));
         assertEquals(0, f.calculateSimpleScore());
     }
@@ -18,6 +19,25 @@ public class FrameTest {
     @Test
     void testTwoTimesFiveHitsShouldReturnSimpleScoreTen(){
         Frame f = new Frame(new Roll(5), new Roll(5));
+        assertEquals(10, f.calculateSimpleScore());
+    }
+
+    @Test
+    void testRandomSpareShouldReturnSimpleScoreTen(){
+        int rand = getRandomNumberInRange(1, 10);
+        // prevent strikes, as rand is used for the first roll
+        while(rand == 10){
+            rand = new Random(10).nextInt() + 1;
+        }
+        Frame f = new Frame(new Roll(rand), new Roll(10 - rand));
+        assertEquals(10, f.calculateSimpleScore());
+    }
+    
+    @Test
+    void testStrikeShouldReturnSimpleScoreTen(){
+        final int RAND = getRandomNumberInRange(1, 10);
+        System.out.println(RAND);
+        Frame f = new Frame(new Roll(10), new Roll(RAND));
         assertEquals(10, f.calculateSimpleScore());
     }
 }
