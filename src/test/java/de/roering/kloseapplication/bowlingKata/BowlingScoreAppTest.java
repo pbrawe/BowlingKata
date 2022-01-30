e15370a9f90ac2b67b103ce0be1b6cb3882db7cf
pbrawe <philipproering94@gmail.com>
2022-01-30 12:56:09 +0100

    move exceptions into own package

--- src/test/java/de/roering/kloseapplication/bowlingKata/BowlingScoreAppTest.java
+++ src/test/java/de/roering/kloseapplication/bowlingKata/BowlingScoreAppTest.java
@@ -1,5 +1,7 @@
 package de.roering.kloseapplication.bowlingKata;
 
+import de.roering.kloseapplication.bowlingKata.exceptions.InvalidRollValueException;
+import de.roering.kloseapplication.bowlingKata.exceptions.UnjustifiedRollException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
